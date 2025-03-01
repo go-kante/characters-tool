fc64f3202cf28000832cd0a2f13476e5f01555da
go-kante <leisk0422@gmail.com>
2025-03-01 15:30:49 +0900

    結果をポストして表示するだけ

---
+++ app/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: '環境依存文字判別ツール' });
+});
+
+router.post('/', function(req, res, next) {
+  res.render('index', { title: '環境依存文字判別ツール' });
+});
+
+module.exports = router;
