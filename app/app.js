f108697fd3d4b047726509f68cf4cf25947ea96f
go-kante <leisk0422@gmail.com>
2025-03-05 00:01:49 +0900

    何故かJSが読み出せる ejsファイルにソースを直接かいた

--- app/app.js
+++ app/app.js
@@ -35,31 +35,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-
- // 色変えるロジック
-var character = ['aiueo'] // 環境依存文字の配列
-if (typeof window !== "undefined" && typeof document !== "undefined") {
-  const editableBox = document.getElementById('floatingTextarea2');
-  console.log(editableBox);}// 入力されたテキスト
-if (typeof window !== "undefined" && typeof document !== "undefined") {
-  const output = document.getElementById('output');
-  console.log(output);}
-  
-if (typeof window !== "undefined" && typeof document !== "undefined") {
-  const btn = document.getElementById('btn');
-  console.log(btn);
-  event.preventDefault();
-  const content = editableBox.value;
-  const regex = new RegExp(`[${character.join('')}]`, 'g'); // 配列を正規表現に変換
-
-  if (regex.test(content)) {
-    output.textContent = "環境依存文字が含まれています。";
-    output.style.color = "red";
-  } else {
-    output.textContent = "環境依存文字は含まれていません。";
-    output.style.color = "green";
-    }
-};
-
-
 module.exports = app;
