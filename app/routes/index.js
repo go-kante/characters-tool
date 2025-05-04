07be364982bb5ca04c12eb9f08f435ac384712c6
go-kante <leisk0422@gmail.com>
2025-05-04 18:18:03 +0900

    サインアップページが表示

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,6 +9,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 app.use('/public', express.static('public'))
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'kanta',
+  password: 'sk0422OK',
+  database: 'dependent_character'
+});
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return
+  }
+  console.log('success');
+});
 
 
 /* GET home page. */
@@ -28,6 +41,6 @@ router.post('/admin', function(req, res, next) {
   res.render('admin', { title: '管理者画面' });
 });
 
-
+router.use('/signup', require('./signup'));
 
 module.exports = router;
